5c90166550ff5ad37f89794518b08e84cc9d43fc
oack7426 <oack7426@gmail.com>
2020-09-13 21:00:10 +0800

    week8

--- test.js
+++ test.js
@@ -1,40 +1,23 @@
-// 聖誕樹
-// * 基數 2n+1
-// ｜ 2n-1
-
-var readline = require('readline');
-var rl = readline.createInterface({
-    input: process.stdin
-});
-
-var lines = []
-
-// 讀取到一行，先把這一行加進去 lines 陣列，最後再一起處理
-rl.on('line', function (line) {
-    lines.push(line)
-});
-
-// 輸入結束，開始針對 lines 做處理
-rl.on('close', function () {
-    solve(lines)
-})
-
-function multiplication(nums) {
-    var numList = nums[0].split(" ");
-    for (var i = Number(numList[0]); i <= Number(numList[1]); i++) {
-        var strNum = String(i);
-        var len = strNum.length;
-        var num = 0;
-        for (var j = 0; j < len; j++) {
-            num += Math.pow(Number(strNum[j]), len);
+const request = new XMLHttpRequest();
+request.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
+        const reponse = request.responseText;
+        const json = JSON.parse(reponse); // 把一個JSON字串轉換成 JavaScript的數值或是物件
+        const users = json.data;
+        const body = document.querySelector('body');
+        for (var i = 0; i < users.length; i++) {
+            const div = document.createElement('div');
+            div.className = "box";
+            div.innerHTML = `<p>${users[i].first_name} ${users[i].last_name}</p>
+            <img src="${users[i].avatar}" alt="${users[i].first_name} ${users[i].last_name}">`;
+            body.appendChild(div);
         }
-        if (num === i) {
-            console.log(i);
-        }
+    } else {
+        console.log('err');
     }
 }
-
-// 上面都不用管，只需要完成這個 function 就好，可以透過 lines[i] 拿取內容
-function solve(num) {
-    multiplication(num);
-}
+request.onerror = function () {
+    console.log('error');
+};
+request.open('GET', 'https://reqres.in/api/users', true);
+request.send();
